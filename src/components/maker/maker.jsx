4755c8d64ae922903d4746e29d0058f514871238
Jason Han <kyubumh@uci.edu>
2021-04-15 04:19:37 -0700

    add editor and preview layout

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -3,6 +3,8 @@ import Footer from '../footer/footer';
 import Header from '../header/header';
 import styles from './maker.module.css';
 import { useHistory } from 'react-router';
+import Editor from '../editor/editor';
+import Preview from '../preview/preview';
 
 const Maker = (props) => {
     const history= useHistory();
@@ -24,6 +26,10 @@ const Maker = (props) => {
     return (
         <section className={styles.maker}>
             <Header onLogout={Logout}/>
+            <div className={styles.container}>
+                <Editor />
+                <Preview />
+            </div>
             <Footer />
         </section>
     )
