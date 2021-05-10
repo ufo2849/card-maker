23bf7a5c3490cd097e4bf6c6767d7ed47b3e8cf9
Jason Han <kyubumh@uci.edu>
2021-05-09 22:35:26 -0700

    new onchange function

--- src/components/image_file_input/image_file_input.jsx
+++ src/components/image_file_input/image_file_input.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styles from './image_file_input.module.css';
 
 const Image_file_input = (props) => {
+    const inputRef= useRef();
+    const onButtonClick= (event) => {
+        event.preventDefault();
+        inputRef.current.click();
+    };
+
+    const onChange = (event) => {
+        
+    }
+
     return (
         <div className={styles.container}>
             <input className={styles.input} 
-            type="file" accept="image/*" name="file"></input>
+            ref= {inputRef}
+            type="file" 
+            accept="image/*" 
+            name="file"
+            onChange= {onChange}>
+            </input>
             <button className={styles.button} onClick={onButtonClick}>
                 {props.name || "No file"}</button>
         </div>
