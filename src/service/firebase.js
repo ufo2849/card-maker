61a05a111b8b4539d52dd67cf66cbb2738d38405
Jason Han <kyubumh@uci.edu>
2021-05-17 16:23:57 -0700

    added firebase databaseurl

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -4,6 +4,7 @@ const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    databaseURL: process.env.REACT_APP_FIREBASE_DB_URL
 };
   // Initialize Firebase
 const firebaseApp= firebase.initializeApp(firebaseConfig);
