2b0c3a19c2825e2831fd46d95044a2cf98a30d36
Jason Han <kyubumh@uci.edu>
2021-05-18 23:35:10 -0700

    syncCard function

--- src/service/card_repository.js
+++ src/service/card_repository.js
@@ -8,6 +8,17 @@ class CardRepository {
     removeCard(userId, card) {
         firebaseApp.database().ref(`${userId}/cards/${card.id}`).remove();
     }
+
+    syncCard(userId, onUpdate) {
+        const CountRef= firebaseApp.database().ref(`${userId}/cards`);
+        CountRef.on('value', (snapshot) => {
+            const value= snapshot.val();
+            if (value) {
+                onUpdate(value);
+            }
+        })
+        return () => CountRef.off();
+    }
 }
 
 export default CardRepository;
